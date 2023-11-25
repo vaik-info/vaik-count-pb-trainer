6eb86d420b85755bed0a67a38c89320aff9bfa3d
k-wakisaka <k.wakisaka@vaikobo.com>
2023-11-25 16:57:46 +0900

    first commit

--- model/mobile_net_v2_cam_model.py
+++ model/mobile_net_v2_cam_model.py
@@ -5,8 +5,8 @@ def prepare(class_num, image_size=(224, 224), bottle_neck=64, fine=False):
                                                    include_top=False, input_shape=(image_size, image_size, 3))
     x = base_model.output
     cam_output = tf.keras.layers.Conv2D(filters=bottle_neck, kernel_size=3, activation='relu')(x)
-    x = tf.keras.layers.GlobalAveragePooling2D()(cam_output)
-    predictions = tf.keras.layers.Dense(class_num, activation='relu')(x)
+    cam_output = tf.keras.layers.Conv2D(filters=class_num, kernel_size=3, activation='relu')(cam_output)
+    predictions = tf.keras.layers.Lambda(lambda x: tf.reduce_sum(x, axis=[1, 2]))(cam_output)
     train_model = tf.keras.Model(inputs=base_model.input, outputs=predictions)
     save_model = tf.keras.Model(inputs=base_model.input, outputs=[predictions, cam_output])
     return train_model, save_model
