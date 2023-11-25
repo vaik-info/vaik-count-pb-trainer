60b47d3caee71a6e7926d6236668d6cb047a45d2
k-wakisaka <k.wakisaka@vaikobo.com>
2023-11-25 16:19:31 +0900

    first commit

--- train_pb.py
+++ train_pb.py
@@ -9,13 +9,13 @@ tf.get_logger().setLevel('ERROR')
 tf.debugging.disable_traceback_filtering()
 
 from data import count_dataset
-from model import mobile_net_v2_model
+from model import mobile_net_v2_cam_model
 from callbacks import save_callback
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 
 model_dict = {
-    'mobile_net_v2_model': mobile_net_v2_model.prepare
+    'mobile_net_v2_cam_model': mobile_net_v2_cam_model.prepare
 }
 
 def train(train_input_dir_path, valid_input_dir_path, classes_txt_path, model_type, epochs, step_size, batch_size,
@@ -39,16 +39,16 @@ def train(train_input_dir_path, valid_input_dir_path, classes_txt_path, model_ty
     train_valid_data = count_dataset.TestCountDataset.get_all_data(train_valid_dataset)
 
     # prepare model
-    model = model_dict[model_type](len(classes), image_size, fine=True)
-    model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.Huber())
+    train_model, save_model = model_dict[model_type](len(classes), image_size, fine=True)
+    train_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), loss=tf.keras.losses.Huber())
 
     # prepare callback
     save_model_dir_path = os.path.join(output_dir_path,
                                        f'{datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%Y-%m-%d-%H-%M-%S")}')
     prefix = f'step-{step_size}_batch-{batch_size}'
-    callback = save_callback.SaveCallback(save_model_dir_path=save_model_dir_path, prefix=prefix, valid_data=valid_data, train_valid_data=train_valid_data)
+    callback = save_callback.SaveCallback(save_model=save_model, save_model_dir_path=save_model_dir_path, prefix=prefix, valid_data=valid_data, train_valid_data=train_valid_data)
 
-    model.fit_generator(train_dataset, steps_per_epoch=step_size,
+    train_model.fit_generator(train_dataset, steps_per_epoch=step_size,
                         epochs=epochs,
                         validation_data=valid_data,
                         callbacks=[callback])
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     parser.add_argument('--train_input_dir_path', type=str, default='~/.vaik-mnist-count-dataset/train')
     parser.add_argument('--valid_input_dir_path', type=str, default='~/.vaik-mnist-count-dataset/valid')
     parser.add_argument('--classes_txt_path', type=str, default='~/.vaik-mnist-count-dataset/classes.txt')
-    parser.add_argument('--model_type', type=str, default='mobile_net_v2_model')
+    parser.add_argument('--model_type', type=str, default='mobile_net_v2_cam_model')
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--step_size', type=int, default=500)
     parser.add_argument('--batch_size', type=int, default=32)
