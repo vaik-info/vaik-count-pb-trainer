ee0779af5281230234255e27374b7970d8a489d9
k-wakisaka <k.wakisaka@vaikobo.com>
2023-11-27 11:02:26 +0900

    first commit

--- model/mobile_net_v2_cam_model.py
+++ model/mobile_net_v2_cam_model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def prepare(class_num, image_size=(224, 224), bottle_neck=64, fine=False):
+def prepare(class_num, image_size=320, bottle_neck=64, fine=False):
     base_model = tf.keras.applications.MobileNetV2(weights='imagenet' if fine else None,
                                                    include_top=False, input_shape=(image_size, image_size, 3))
     #x = base_model.layers[56].output
